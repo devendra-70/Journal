8a1e3433dccd9aea5f421df2360a54e49a5d5749
devendra-70 <devendramahajan70@gmail.com>
2025-08-16 00:13:17 +0530

    added mongodb config

--- src/main/java/com/starter/journal/controller/JournalEntryController.java
+++ src/main/java/com/starter/journal/controller/JournalEntryController.java
@@ -1,20 +1,44 @@
 package com.starter.journal.controller;
 
 import com.starter.journal.entity.JournalEntry;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("entries")
 public class JournalEntryController {
 
     private Map<Long,JournalEntry> journalEntries=new HashMap<>();
 
-
-    public Map<Long,JournalEntry> getAll(){
+    @GetMapping
+    public Map<Long,JournalEntry> getAllEntries(){
         return journalEntries;
     }
-}
+
+    @GetMapping("id/{requestedId}")
+    public JournalEntry getEntryById(@PathVariable Long requestedId){
+        return journalEntries.get(requestedId);
+    }
+
+    @PostMapping
+    public JournalEntry createEntry(@RequestBody JournalEntry newEntry){
+        journalEntries.put(newEntry.getId(),newEntry);
+            return journalEntries.get(newEntry.getId());
+    }
+
+    @DeleteMapping("delete/{requestedId}")
+    public boolean deleteEntry(@PathVariable Long requestedId){
+        journalEntries.remove(requestedId);
+        return journalEntries.get(requestedId) == null;
+    }
+
+    @PutMapping("update/{requestedId}")
+    public JournalEntry updateEntry(@PathVariable Long requestedId,@RequestBody JournalEntry requestedEntry){
+        return journalEntries.put(requestedId,requestedEntry);
+    }
+
+
+    }
+
